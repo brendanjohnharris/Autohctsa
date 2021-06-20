dd7a8721ef6cbb887b1c59372d2f220a4af8a02b
brendanjohnharris <brendanjohnharris@gmail.com>
2021-06-20 19:34:35 +1000

    Add __init__ and function to retrieve completed calculations

--- src/autohctsa.jl
+++ src/autohctsa.jl
@@ -1,5 +1,6 @@
 module Autohctsa
 using Preferences
+using CSV
 
 function sethostname(hostname::String)
     @set_preferences!("hostname" => hostname)
@@ -12,32 +13,83 @@ function setremotedir(remotedir::String)
 end
 const remotedir = @load_preference("remotedir", nothing)
 
+function __init__()
+    if isnothing(hostname)
+        @warn "No default hostname set. Use `sethostname(hostname::String)` to do so."
+    end
+    if isnothing(remotedir)
+        @warn "No default remotedir set. Use `setremotedir(hostname::String)` to do so."
+    end
+
+    # Try connecting to remote with defaults
+    shcmd = `ssh $hostname "pwd"`
+    f() = run(shcmd)
+    go = Task(f)
+    schedule(go)
+    g() = istaskdone(go)
+    check = timedwait(g, 5)
+    if check != :ok
+        println("")
+        @warn "Could not automaticaly connect to default remote. Please check preferences, and try connecting to `hostname` manually. You might need to set up a key pair so that password authentication is not required."
+    else
+        @info "Automatic connection to default hostname successful!"
+    end
+end
+
+
 
 """
 This one just wraps the shell script
 """
 function autohctsa(in::String, out::String, hostname::String)
+    inn = abspath(in)
     sh = abspath(@__DIR__, "autohctsa.sh")
-    shcmd = `$sh -i $in -o $out -h $hostname`
-    run(shcmd)
+    hm = pwd()
+    cd(@__DIR__)
+    try
+        shcmd = `"$sh" -i "$inn" -o "$out" -h "$hostname"`
+        run(shcmd)
+    catch e
+        cd(hm)
+        throw(e)
+    end
+    cd(hm)
 end
 
 """
 This one takes an input file, submits the job to the cluster and saves the result in the remote directory and saves placeholder files locally. Once the remote job is complete, the placeholder files can be retrieved using `retrieve()`.
 """
-function submit(in::String, outfile::String="hctsa_"*match(r"[^_]+(?=\.)", basename(in))*".csv"; remotedir=remotedir, hostname=hostname)
+function submit(in::String, outfile::String="hctsa_"*match(r"[^_]+(?=\.)", basename(in)).match*".csv"; remotedir=remotedir, hostname=hostname)
     isnothing(remotedir) ? (@error "Please provide an absolute path to a remote directory, or set a default") : nothing
     isnothing(hostname) ? (@error "Please provide a `<user>@<hostname>` or set a default") : nothing
     out = joinpath(remotedir, outfile)
     autohctsa(in, out, hostname)
+    tmp = abspath(dirname(in), splitext(outfile)[1]*".autohctsa")
+    tmpdict = Dict(:hostname=>hostname, :in=>in, :out=>out, :tmp=>tmp)
+    CSV.write(tmp, tmpdict)
 end
 
 
 """
-Get completed calculations from the cluster, according to any local placeholder files in the specified directory or its subdirectories
+Get completed calculations from the cluster, according to any local placeholder (.autohctsa) files in the specified directory or its subdirectories
 """
-function retrieve()
-
+function retrieve(dir::String; remotedir=remotedir, hostname=hostname)
+    # We will walk the given directory recursively for files that have the extension .autohctsa
+    remotefs = readlines(`ssh $hostname "find $remotedir -name \* -print"`)
+    for (root, dirs, files) in walkdir(dir)
+        for f ∈ files
+            if splitext(f)[2] == ".autohctsa"
+                D = CSV.File(abspath(root, f)) |> Dict
+                lookingfor = splitext(basename(f))[1]*".csv"
+                if hostname == D["hostname"] && dirname(remotedir*"/") == dirname(D["out"]) && any(lookingfor .== basename.(remotefs))
+                    dest = abspath(root, lookingfor)
+                    remff = joinpath(remotedir, lookingfor)
+                    run(`scp "$hostname:$remff" "$dest"`)
+                    rm(splitext(dest)[1]*".autohctsa")
+                end
+            end
+        end
+    end
 end
 
 
